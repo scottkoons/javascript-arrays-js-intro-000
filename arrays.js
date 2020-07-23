f07865c5e58fa447d67a908fe3a38570ef95d2ac
communal-documentation-0347 <scottkoons@gmail.com>
2020-07-23 22:06:43 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -32,3 +32,8 @@ function removeElementFromBeginningOfArray(array) {
   array = array.slice(1);
   return array;
 }
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+}
